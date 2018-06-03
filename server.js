43c4253ddef5c14fc68a90e973aeab0c788d3301
DennisIlyaev <DennisIlyaev@yahoo.com>
2018-06-03 22:08:30 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
-const cors = require('cors');
 const app = express();
 const api = require('./routes/api');
 const port = process.env.PORT;
 
-app.use(cors());
+
 app.use(express.static(path.join(__dirname, 'dist')));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
